de61d0e33bf3a46be025a3399e5275ae37db4e9c
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2025-05-04 22:27:48 +0300

    Apply Migration

--- Sports-Playlist-Server/Models/Playlist.cs
+++ Sports-Playlist-Server/Models/Playlist.cs
@@ -8,7 +8,7 @@ namespace Sports_Playlist_Server.Models
     public class Playlist
     {
         public int Id { get; set; }
-        public Guid UserId { get; set; }
+        public string UserId { get; set; }
         public User User { get; set; }
         public int MatchId { get; set; }
         public Match Match { get; set; }
