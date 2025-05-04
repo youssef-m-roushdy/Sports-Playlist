575fa63b650436420de054498dadbc5b9ea707e8
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2025-05-04 22:23:09 +0300

    Use Sql Server for data storing

--- Sports-Playlist-Server/Program.cs
+++ Sports-Playlist-Server/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Sports_Playlist_Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("MatchesDb")
+    )
+);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
