d9b5fbc1986e1ae15f0f0703742a61eb8dd81999
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2025-05-04 23:19:54 +0300

    Generic Repository

---
+++ Sports-Playlist-Server/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sports_Playlist_Server.Interfaces
+{
+
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
